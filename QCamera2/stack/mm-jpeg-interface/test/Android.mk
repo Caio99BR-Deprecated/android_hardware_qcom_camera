574ab273e2b600e06e20f152af2e4accf834fc40
Ying Wang <wangying@google.com>
2014-12-16 12:31:13 -0800

    Cleanup Obsolete LOCAL_PRELINK_MODULE.
    Bug: 18675947
    Change-Id: Ic00a367b5c108f4a7c18b3ab4d3a1da74cc97353

--- QCamera2/stack/mm-jpeg-interface/test/Android.mk
+++ QCamera2/stack/mm-jpeg-interface/test/Android.mk
@@ -39,7 +39,6 @@ LOCAL_SRC_FILES := mm_jpeg_test.c
 LOCAL_CLANG := false
 
 LOCAL_MODULE           := mm-jpeg-interface-test
-LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libcutils libdl libmmjpeg_interface
 
 #include $(BUILD_EXECUTABLE)
@@ -85,7 +84,6 @@ LOCAL_SRC_FILES := mm_jpegdec_test.c
 LOCAL_CLANG := false
 
 LOCAL_MODULE           := mm-jpegdec-interface-test
-LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libcutils libdl libmmjpeg_interface
 
 #include $(BUILD_EXECUTABLE)
