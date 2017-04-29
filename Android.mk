62ddefadf95c4efcb76d655917b09e3b60c8023e
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-04-29 16:56:36 -0300

    Android.mk: Squashed rework commits
    * Remove dependency on is-board-platform-in-list to determine target
    * Put back USE_DEVICE_SPECIFIC_CAMERA
    * Use project pathmap
    * Only allow B Family
    * Remove deprecated BUILD_TINY_ANDROID
    
    Change-Id: I2a6fc731a1a080618bab54ebbe4783d36429ad81
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,14 +1,9 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 
-ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
-  ifneq ($(USE_CAMERA_STUB),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+  ifneq ($(filter msm8226 msm8610 msm8974,$(TARGET_BOARD_PLATFORM)),)
+    include $(call all-subdir-makefiles)
   endif
+endif
+
 endif
