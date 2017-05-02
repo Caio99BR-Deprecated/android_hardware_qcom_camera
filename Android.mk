84e36225a1ebb5502d033a25f64b477b6742cc7b
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-05-01 22:14:21 -0300

    Android.mk: Fix building
    Change-Id: I680f8791bd5feb8855110b8928f29d298e512e61
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,9 +1,5 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
-
 ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
   ifneq ($(filter msm8226 msm8610 msm8974,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-subdir-makefiles)
   endif
-endif
-
 endif
